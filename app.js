d0b81fe6a4887a0da903a0df61ff17ac51c4104a
Taylor Jordan <taylornoel418@icloud.com>
2019-08-13 20:33:29 -0600

    Add read all and read one routes

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const morgan = require('morgan');
+const dbConnection = require('./connection');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/api/v1/crystals', (req, res) => res.send('Hello Crystals!'))
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'));
+
+app.get('/api/v1/crystals', (req, res) => {
+    dbConnection('crystal')
+        .select('*')
+        .then(crystals => res.status(200).json(crystals))
+        .catch(error => res.json({error: error.message, stack: error.stack}));
+});
+
+app.get('/api/v1/crystals/:id', (req, res) => {
+    dbConnection('crystal')
+        .select('*')
+        .limit(1)
+        .where({ id: req.params.id })
+        .then(crystal => res.status(200).json(crystal))
+        .catch(error => res.json({error: error.message, stack: error.stack}));
+
+})
+
 
 app.listen(PORT, () => console.log(`Example app running!`))
