3f4866024f231023227536fa02d9583bd604935c
Taylor Jordan <taylornoel418@icloud.com>
2019-08-18 21:33:40 -0600

    Update production in knex file

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,17 @@
+    
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost:/passport',
+    client: "pg",
+    connection: "postgres://localhost:/passport",
     migrations: {
-      directory: './migrations'
+      directory: "./migrations"
     },
-    seeds: {
-      directory: './seeds/dev'
-    }
+    useNullAsDefault: true
   },
-
-  production: { 
+  seeds: {
+    directory: "./seeds/dev"
+  },
+  production: {
     client: "pg",
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
